f4bc599cc4d40cf86cc26045464d422ffc686a1d
Rodel Castro <m88.shang.rodeca@gmail.com>
2023-08-15 15:34:16 +0800

    DG-0001 Initial project commit

---
+++ src/test/java/com/sss/products/merge/MergeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sss.products.merge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
